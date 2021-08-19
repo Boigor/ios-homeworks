e739fdcde00e5d64824b7fdce8be62b574d44425
Boigor <87991134+Boigor@users.noreply.github.com>
2021-08-19 23:44:59 +0300

    Initial Commit

---
+++ Shared/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Shared
+//
+//  Created by Igor Markiv on 19.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
